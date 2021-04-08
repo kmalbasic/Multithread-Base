49ee1ffc02309e283253574c55230ef5a9bb0fd3
cocaine666 <63399143+cocaine666@users.noreply.github.com>
2021-04-09 01:47:01 +0200

    Update threadlib.hpp

--- threadlib.hpp
+++ threadlib.hpp
@@ -12,9 +12,11 @@ struct thread_info {
 };
 
 namespace thread_lib {
+	
 	std::string get_cpu_name();
 	int calculate_threads();
 	thread_info initialize_info();
 	void print_info(thread_info thread_object);
 	void run_workload(thread_info thread_object, thread threads[]);
+	
 }

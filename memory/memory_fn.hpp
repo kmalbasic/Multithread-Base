82e2ec05bb34952e66be0c5be9f61bec232e932e
cocaine666 <63399143+cocaine666@users.noreply.github.com>
2021-04-29 09:04:37 -0700

    Add files via upload

---
+++ memory/memory_fn.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+
+namespace memory {
+
+	// These functions are in here for intermediate programmers, mostly people that are into game-hacking.
+	// In future there will be some pattern scanning functions and possibly implementation of a communicating
+	// with a driver to do some WPM/RPM through kernel.
+
+	bool attach(const char* process_name, DWORD access_rights);
+	bool detach(HANDLE h);
+
+	template <typename t>
+	t read_memory(DWORD address);
+
+	template <typename t>
+	void write_memory(DWORD address, t value);
+
+}
